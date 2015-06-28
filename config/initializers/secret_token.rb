5b0b9f24f09115059ca1b03cc66ebb22f0b3dfa6
刘同彬 <liutongbin@souche.com>
2015-06-28 22:14:02 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewGroup::Application.config.secret_key_base = 'f2c8786701d9cf2afa60fd760c1cb652433b1a1af6b44b50e152d89bd3b061f5c1cd0713703ea99991d201a3c80236cc19bab930247f4558abad12b53a306d5a'
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+NewGroup::Application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  #root :to => 'high_voltage/pages#show', :id => 'welcome'
+  root :to => "groups#index"
+  resources :groups do
+ 	 resources :posts
+  end
+end
